7327d45ed65c8aa44e3240821a7a852f2a43c520
info-coe <coeteam@infomericainc.in>
2024-10-08 14:03:16 +0530

    pages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import RewardsIntro from './components/RewardsIntro';
+import RewardsLogin from './components/RewardsLogin';
+import RewardsOtpVerification from './components/RewardsOtpVerification';
+import RewardsCreateAccount from './components/RewardsCreateAccount';
+import RewardsHome from './components/RewardsHome';
+import RewardsTier from './components/RewardsTier';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RewardsIntro />} />
+        <Route path='login' element={< RewardsLogin/>} />
+        <Route path='userverification' element={<RewardsOtpVerification />} />
+        <Route path='createaccount' element={<RewardsCreateAccount />} />
+        <Route path='rewardshome' element={<RewardsHome />} />
+        <Route path='tier' element={<RewardsTier />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
